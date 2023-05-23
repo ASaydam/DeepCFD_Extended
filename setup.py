136a20725631cc382c21fd5b92a0385e09feaf45
Alper Saydam <asaydam21@ku.edu.tr>
2023-05-23 17:21:19 +0300

    setup name changed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/mdribeiro/DeepCFD',
-    packages=['deepcfd-extended', 'deepcfd.models'],
+    packages=['deepcfd', 'deepcfd.models'],
     package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
